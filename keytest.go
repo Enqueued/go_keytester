eb90bd7da0e1d28dea2163ba974ec8e58a87b33f
UltimaQuazar <gcw66@protonmail.com>
2018-04-19 07:36:36 -0500

    starting on new project should probably open a different repo but i dont want to...

--- keytest.go
+++ keytest.go
@@ -4,15 +4,20 @@
    to the xbindkeys to just check.
 */
 package main
-import ("fmt")//; "bufio"; "os")
+
+import (
+	"fmt"
+) //; "bufio"; "os")
 
 /*
   Issue number 1: it seems like go uses UTF-8 as the go-to encoding model
   gotta see about if a default library does exist.
   doesnt seem to be one for reading in and holding the keyboard hostage...
 */
-func main(){
-  asciiS := "ABC"
-  ascNum := []byte(asciiS)
-  fmt.Printf("OK: String = %d", ascNum[1])
+func main() {
+
+	asciiS := "ABC"
+	ascNum := []byte(asciiS)
+	fmt.Printf("OK: String = %d", ascNum[1])
+
 }
--- masta_back.go
+++ masta_back.go
@@ -1,27 +1,38 @@
 package main
 import (
-	"fmt"
-	"net/http"
-	"io/ioutil"
-	"os"
-	//"golang.org/x/net/html"
-	)
+    "fmt"; "net/http"
+    "io/ioutil"; "os"
+    "bufio"; "strings"
+    //"golang.org/x/net/html"
+    )
+
+func err_chk(err error, msg string){
+    if err != nil{
+        fmt.Println("Error Code: "+msg)
+        os.Exit(-1)
+    }
+}
+
+func scrape(url string) []byte{
+    resp, err := http.Get("http://"+url)
+    err_chk(err, "g10")
+
+    bytes, err := ioutil.ReadAll(resp.Body)
+    err_chk(err, "i04")
+    resp.Body.Close()
+
+    return bytes
+}
 
 func main(){
-	fmt.Println("Hello der")
-	resp, err := http.Get("https://www.google.com")
-	
-	if err != nil{
-		fmt.Println("error code: g10")
-		os.Exit(-1)
-	}
-	bytes, err := ioutil.ReadAll(resp.Body)
-
-	if err != nil{
-		fmt.Println("error code: ioR")
-		os.Exit(-1)
-	}
-
-	fmt.Println("HTML:\n\n", string(bytes))
-	resp.Body.Close()
+    fmt.Println("enter a url")
+    reader := bufio.NewReader(os.Stdin)
+    text, err := reader.ReadString('\n')
+    //error check the string
+    err_chk(err, "t10")
+    text = strings.Trim(text, "\n")
+    
+    fmt.Println(text)
+    out := scrape(text)
+    fmt.Println("HTML:\n\n", string(out))
 }
